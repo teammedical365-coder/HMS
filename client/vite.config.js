4c663d7fbba076418dd287e324a78e5aa7a19f23
Harsha-391 <143133227+Harsha-391@users.noreply.github.com>
2026-02-19 15:11:26 +0530

    jghckhjgfkhjgfc

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,20 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    optimizeDeps: {
-        include: ['jspdf', 'jspdf-autotable'],
-    },
+
+    // --- ADD THIS BUILD SECTION TO FIX REDUX ---
     build: {
         commonjsOptions: {
-            include: [/jspdf/, /jspdf-autotable/],
+            transformMixedEsModules: true,
+            include: [/use-sync-external-store/, /node_modules/],
         },
     },
+    // -------------------------------------------
+
     server: {
         port: 5173,
-        // Add this proxy configuration to connect to Backend
         proxy: {
             '/api': {
-                target: 'http://localhost:3000', // Matches your server.js PORT
+                target: 'http://localhost:3000',
                 changeOrigin: true,
                 secure: false,
             },
